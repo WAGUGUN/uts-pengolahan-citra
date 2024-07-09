a57a4df5653b621845db5c046c43169766585adc
gugunwell <115972485+WAGUGUN@users.noreply.github.com>
2024-07-09 21:35:33 +0700

    first upload
    first

---
+++ home.py
@@ -0,0 +1,10 @@
+import streamlit as st
+
+st.title('Biodata Diri')
+st.subheader('Nama : Gugun Gunawan')
+st.subheader('NIM : 312210280')
+st.subheader('Kelas : TI.22.B1')
+st.subheader('Prodi : Teknik Infomatika')
+
+st.image("./img/gugun.jpg", caption="Gugun", use_column_width="Always", width=200)
+
